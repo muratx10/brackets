d8d6523e753a40201f0ebf1ca00caaa2915527e9
Murat AKMAMEDAU <muratx10@gmail.com>
2019-09-22 21:20:49 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let regexp = /\(\)|\{\}|\[\]|\|\||[1][2]|[3][4]|[5][6]|[7][7]|[8][8]/;
+    let flag;
+    const trim = function (str) {
+        const newStr = str.replace(regexp, "");
+        if ((newStr.length !== 0) && (regexp.test(str))) {
+            trim(newStr);
+        } else if (newStr.length == 0) {
+            return (flag = true);
+        } else {
+            return (flag = false);
+        }
+    };
+    trim(str);
+    return flag;
 }
